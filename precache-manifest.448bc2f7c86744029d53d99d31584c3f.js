69c89604c57999503e3a7c7f23338dbfd4ce0bc5
Gabriela Carvalho Camilo <gabriela.camilo@pca.com.br>
2019-10-29 15:17:19 -0300

    Updates

---
+++ precache-manifest.448bc2f7c86744029d53d99d31584c3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e78947de7ab5fd7c4f1b65bb501cd62",
+    "url": "/roboFriends/index.html"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/roboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "82fbb8e777f34aae7666",
+    "url": "/roboFriends/static/css/main.5068b41b.chunk.css"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/roboFriends/static/js/2.a09a1be0.chunk.js"
+  },
+  {
+    "revision": "82fbb8e777f34aae7666",
+    "url": "/roboFriends/static/js/main.74acd0d7.chunk.js"
+  },
+  {
+    "revision": "a7e456327f9ffa7e2432",
+    "url": "/roboFriends/static/js/runtime-main.595aa053.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
